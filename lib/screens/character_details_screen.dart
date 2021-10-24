b1b3ba42cb5a6c0a7212b68824c8399c085d6b7a
EmmilioV <emmiliov.p@gmail.com>
2021-10-23 21:21:40 -0500

    improve character_details_screen, all information of character completed and showed

--- lib/screens/character_details_screen.dart
+++ lib/screens/character_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:pablo_villa_4_2021_2_p1/models/character.dart';
 import 'package:pablo_villa_4_2021_2_p1/models/psi_power.dart';
@@ -18,7 +19,15 @@ class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.character.name)
+          title: Center(
+            child: Text(
+              widget.character.name,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ) 
+          )
       ),
       body: Center(
         child: _showCharacterDetails(),
@@ -32,9 +41,50 @@ class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
         children:
         [
           Image.network(widget.character.img),
-          Text('Gender => ${widget.character.gender}'),
+          Text(
+            widget.character.name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            ),
+          ),
+          Text(
+            widget.character.gender,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            )
+          ),
+          Container(
+            child: _showPowersInTable(),
+          )
         ]
       )
+    );
+  }
+
+  Widget _showPowersInTable() {
+    return DataTable(sortColumnIndex: 2,
+      sortAscending: false,
+      columns: [
+        DataColumn(label: Text("Power")),
+        DataColumn(label: Text("Description")),
+        DataColumn(label: Text("image"), numeric: true),
+      ],
+      rows:
+        widget.character.psiPowers.map((e) => DataRow(
+            cells: [
+              DataCell(
+                Text(e.name)
+              ),
+              DataCell(
+                Text(e.description)
+              ),
+              DataCell(
+                Image.network(e.img)
+              ),
+            ])
+      ).toList(),
     );
   }
 }
